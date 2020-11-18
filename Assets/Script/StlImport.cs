162edfe7566dd3d4c5460366a59e03757bed6cb9
babypink <jason.poh.hwa.lai@uni-weimar.de>
2020-11-18 01:25:52 +0100

    add in STL importer

---
+++ Assets/Script/StlImport.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Parabox.Stl;
+using System.IO;
+
+public class StlImport : MonoBehaviour
+{
+    public string filePath;
+
+    [EasyButtons.Button]
+    private void importSTL()
+    {
+        filePath = EditorUtility.OpenFilePanel("Open with stl", "", "stl");
+        if (filePath.Length != 0)
+        {
+            GameObject root = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            root.name = Path.GetFileNameWithoutExtension(filePath);
+
+            MeshFilter m = root.GetComponent<MeshFilter>();
+
+            Mesh[] mesh = Importer.Import(filePath);
+
+            m.mesh = mesh[0];
+        }
+    }
+}

e9d6ce72f05022c3f71af32806cf7678cf2a8165
local user <user@ITD-061.bauing.uni-weimar.de>
2020-12-11 19:39:41 +0100

    add in 3D point construction

---
+++ Assets/Script/Arrow_Ctrl.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Arrow_Ctrl : MonoBehaviour
+{
+    public float dragSpeed = 10.0f;
+    public GameObject Highlight;
+    public GameObject Single_Axis_Control;
+    private Vector3 prev_pos;
+
+    public delegate void Dragged(object sender, Vector3 position);
+
+    public event Dragged OnDragged;
+
+    void Start()
+    {
+        Highlight.SetActive(false);
+    }
+
+    public void Activate(bool active)
+    {
+        GetComponentInChildren<CapsuleCollider>().enabled = active;
+    }
+
+    void OnMouseEnter()
+    {
+        Highlight.SetActive(true);
+        prev_pos = Input.mousePosition;
+    }
+
+    void OnMouseOver()
+    {
+        Highlight.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        Highlight.SetActive(false);
+    }
+
+    void dragging()
+    {
+        Highlight.SetActive(true);
+        //Debug.Log(System.String.Format("{0}, {1}", Input.mousePosition, prev_pos));
+
+        Vector3 prev_pos_clip = prev_pos;
+        prev_pos_clip.z = Camera.main.nearClipPlane;
+
+        Vector3 current_pos_clip = Input.mousePosition;
+        current_pos_clip.z = Camera.main.nearClipPlane;
+
+        Vector3 world_prev = Camera.main.ScreenToWorldPoint(prev_pos_clip);
+        Vector3 world_current = Camera.main.ScreenToWorldPoint(current_pos_clip);
+
+        float delta = Vector3.Dot(world_current - world_prev, Single_Axis_Control.transform.up);
+        Vector3 pos = Single_Axis_Control.transform.position;
+
+        pos += Single_Axis_Control.transform.up * (delta * dragSpeed);
+        Single_Axis_Control.transform.position = pos;
+        prev_pos = Input.mousePosition;
+
+        OnDragged(this, Single_Axis_Control.transform.position);
+    }
+
+    void OnMouseDrag()
+    {
+        dragging();
+    }
+}
--- Assets/Script/surfaceBuider.cs
+++ Assets/Script/surfaceBuider.cs
@@ -4,25 +4,288 @@ using UnityEngine;
 
 public class surfaceBuider : MonoBehaviour
 {
-    public GameObject plane;
+    List<Surface> surfaces = new List<Surface>();
+    Surface possible_surface;
+    private bool error = false;
 
-    void Update()
+    void Start()
     {
-        if (Input.GetMouseButton(0))
+        StartCoroutine(CreateSurfaces());
+    }
+
+    IEnumerator CreateSurfaces()
+    {
+        yield return CreateSurface();
+        yield return CreateSurface();
+        yield return validate2Surfaces();
+        yield return validate3Surfaces();
+        yield return CreatePoint();
+
+        //foreach (Surface s in surfaces)
+        //    Debug.Log(System.String.Format("Normal: {0}, Position: {1}", s.getNormal(), s.getPosition()));    
+    }
+
+    IEnumerator CreatePoint()
+    {
+        Plane p0 = surfaces[0].getPlane();
+        Plane p1 = surfaces[1].getPlane();
+        Plane p2 = surfaces[2].getPlane();
+
+        Vector3 intersectionPoint;
+        if (planesIntersectAtSinglePoint(p0 : p0, p1: p1, p2: p2, out intersectionPoint))
+            Instantiate(Resources.Load<GameObject>("Prefabs/Pointer"), intersectionPoint, Quaternion.identity);
+
+        yield return null;
+    }
+
+    IEnumerator validate2Surfaces()
+    {
+        while (surfaces[0].getNormal().normalized == surfaces[1].getNormal().normalized)
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            error = true;
+            yield return CreateSurface();
+        }
+        error = false;
+        yield return null;
+    }
 
-            if (Physics.Raycast(ray, out hit))
-            {
-                // Find the line from the gun to the point that was clicked.
-                Vector3 position = hit.point;
-                Vector3 normal = hit.normal;
-                
+    IEnumerator validate3Surfaces()
+    {
+        if (surfaces.Count < 3) yield return CreateSurface();
+        while (surfaces[0].getNormal().normalized == surfaces[2].getNormal().normalized || surfaces[1].getNormal().normalized == surfaces[2].getNormal().normalized)
+        {
+            error = true;
+            yield return CreateSurface();
+        }
+        error = false;
+        yield return null;
+    }
 
-                plane.transform.position = position;
-                plane.transform.rotation = Quaternion.FromToRotation(Vector3.up, normal);
-            }
+    IEnumerator createLine()
+    {
+        Surface s1 = surfaces[0];
+        Surface s2 = surfaces[1];
+
+        Vector3 origin;
+        Vector3 direction;
+
+        if(PlanePlaneIntersection(out origin, out direction, plane1Normal: s1.getNormal(), plane1Position: s1.getPosition(), plane2Normal: s2.getNormal(), plane2Position: s2.getPosition()))
+        {
+            Instantiate(Resources.Load<GameObject>("Prefabs/Double_axis_arrow"), origin, Quaternion.FromToRotation(Vector3.up, direction));
+            possible_surface = new Surface(position: origin, normal: direction);
+        } 
+
+        yield return null;
+    }
+
+    private static bool planesIntersectAtSinglePoint(Plane p0, Plane p1, Plane p2, out Vector3 intersectionPoint)
+    {
+        const float EPSILON = 1e-4f;
+
+        var det = Vector3.Dot(Vector3.Cross(p0.normal, p1.normal), p2.normal);
+        if (det < EPSILON)
+        {
+            intersectionPoint = Vector3.zero;
+            return false;
+        }
+
+        intersectionPoint =
+            (-(p0.distance * Vector3.Cross(p1.normal, p2.normal)) -
+            (p1.distance * Vector3.Cross(p2.normal, p0.normal)) -
+            (p2.distance * Vector3.Cross(p0.normal, p1.normal))) / det;
+
+        return true;
+    }
+
+    public static bool PlanePlaneIntersection(out Vector3 linePoint, out Vector3 lineVec, Vector3 plane1Normal, Vector3 plane1Position, Vector3 plane2Normal, Vector3 plane2Position)
+    {
+
+        linePoint = Vector3.zero;
+        lineVec = Vector3.zero;
+
+        //We can get the direction of the line of intersection of the two planes by calculating the 
+        //cross product of the normals of the two planes. Note that this is just a direction and the line
+        //is not fixed in space yet. We need a point for that to go with the line vector.
+        lineVec = Vector3.Cross(plane1Normal, plane2Normal);
+
+        //Next is to calculate a point on the line to fix it's position in space. This is done by finding a vector from
+        //the plane2 location, moving parallel to it's plane, and intersecting plane1. To prevent rounding
+        //errors, this vector also has to be perpendicular to lineDirection. To get this vector, calculate
+        //the cross product of the normal of plane2 and the lineDirection.		
+        Vector3 ldir = Vector3.Cross(plane2Normal, lineVec);
+
+        float denominator = Vector3.Dot(plane1Normal, ldir);
+
+        //Prevent divide by zero and rounding errors by requiring about 5 degrees angle between the planes.
+        if (Mathf.Abs(denominator) > 0.006f)
+        {
+
+            Vector3 plane1ToPlane2 = plane1Position - plane2Position;
+            float t = Vector3.Dot(plane1Normal, plane1ToPlane2) / denominator;
+            linePoint = plane2Position + t * ldir;
+
+            return true;
+        }
+
+        //output not valid
+        else
+        {
+            return false;
+        }
+    }
+
+    IEnumerator CreateSurface()
+    {
+        GameObject object1 = new GameObject();
+        SurfaceVariable sv = object1.AddComponent<SurfaceVariable>() as SurfaceVariable;
+        sv.OnSurfaceBuilt += TermianteObject;
+
+        while (object1)
+        {
+            yield return null;
+        }
+    }
+
+    void TermianteObject(object sender, Surface surface)
+    {
+        if (error) surfaces[surfaces.Count - 1] = surface;
+        else surfaces.Add(surface);
+
+        SurfaceVariable sv = sender as SurfaceVariable;
+        sv.OnSurfaceBuilt -= TermianteObject;
+        Destroy(sv.gameObject);
+    }
+
+    void OnGUI()
+    {
+        if (error) GUI.Label(new Rect(Screen.width - 200, 50, 200, 50), "Surfaces cannot be of same direction!!");
+    }
+}
+
+public struct Surface
+{
+    Vector3 position;
+    Vector3 normal;
+    Plane plane;
+
+    public Surface(Vector3 position, Vector3 normal)
+    {
+        this.position = position;
+        this.normal = normal;
+        this.plane = new Plane(this.normal, this.position);
+    }
+
+    public Vector3 getPosition()
+    {
+        return this.position;
+    }
+
+    public Vector3 getNormal()
+    {
+        return this.normal;
+    }
+
+    public Plane getPlane()
+    {
+        return this.plane;
+    }
+
+    public void updatePosition(float distance)
+    {
+        this.position += distance * this.normal.normalized;
+        this.plane = new Plane(this.normal, this.position);
+    }
+}
+
+public class SurfaceVariable : MonoBehaviour
+{
+    private bool confirm = false;
+    private bool depth = false;
+    private Surface surface;
+    private GameObject plane;
+    private Arrow_Ctrl arrow_ctrl;
+    private float depth_Value = 0.0f;
+
+    public delegate void surfaceBuilt(object sender, Surface surface);
+    public event surfaceBuilt OnSurfaceBuilt;
+
+    void Start()
+    {
+        if (plane == null) plane = Instantiate(Resources.Load<GameObject>("Prefabs/Plane_Control"));
+        arrow_ctrl = plane.GetComponentInChildren<Arrow_Ctrl>();
+        arrow_ctrl.Activate(false);
+        surface = new Surface(position: Vector3.zero, normal: Vector3.up);
+
+        // Action event Binding
+        arrow_ctrl.OnDragged += calculate;
+    }
+
+
+    private void getSurface()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        int layerMask = 1 << LayerMask.NameToLayer("Control");
+        layerMask = ~layerMask;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        {
+            // Find the line from the gun to the point that was clicked.
+            Vector3 position = hit.point;
+            Vector3 normal = hit.normal;
+
+            plane.transform.SetPositionAndRotation(position, Quaternion.FromToRotation(Vector3.up, normal));
+
+            surface = new Surface(position: position, normal: normal);
+        }
+    }
+
+    void ConfirmSurface()
+    {
+        if (GUI.Button(new Rect(Screen.width - 120, Screen.height / 2, 100, 50), "Confirm\nSurface"))
+        {
+            confirm = true;
+            arrow_ctrl.Activate(true);
+        }
+    }
+
+    void ConfirmDepth()
+    {
+        if (GUI.Button(new Rect(Screen.width - 120, Screen.height / 2, 100, 50), "Confirm\nDepth"))
+        {
+            depth = true;
+            surface.updatePosition(depth_Value);
+            arrow_ctrl.OnDragged -= calculate;
+            Destroy(plane);
+
+            OnSurfaceBuilt(this, surface);
+        }
+    }
+
+    void showDepth()
+    {
+        string depth_String = System.String.Format("{0}", depth_Value);
+        depth_String = GUI.TextField(new Rect(Screen.width - 120, Screen.height / 2 - 100, 100, 30), depth_String, 5);
+    }
+
+    void calculate(object sender, Vector3 position)
+    {
+        depth_Value = Vector3.Distance(surface.getPosition(), position);
+    }
+
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (!confirm)
+        {
+            if (e.button == 0 && e.isMouse) getSurface();
+            ConfirmSurface();
+        }
+        else if (!depth)
+        {
+            showDepth();
+            ConfirmDepth();
         }
     }
 }
